8b3c0a45069d476e548e944db89b1b24f5a18460
katerina <katerina_panina1@mail.ru>
2024-09-01 23:52:52 +0300

    add methods for creation people

--- src/main/java/ru/panina/springproject/controllers/PeopleController.java
+++ src/main/java/ru/panina/springproject/controllers/PeopleController.java
@@ -1,11 +1,16 @@
 package ru.panina.springproject.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.panina.springproject.dao.PersonDAO;
+import ru.panina.springproject.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -19,5 +24,18 @@ public class PeopleController {
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
+    }
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "people/new";
+        }
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }

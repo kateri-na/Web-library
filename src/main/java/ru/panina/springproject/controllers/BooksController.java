d8e8439b680fe0e4fdd3f80fc25290b356a6009d
katerina <katerina_panina1@mail.ru>
2024-09-02 13:56:29 +0300

    injected dependency from dao and add method for index page

--- src/main/java/ru/panina/springproject/controllers/BooksController.java
+++ src/main/java/ru/panina/springproject/controllers/BooksController.java
@@ -1,7 +1,24 @@
 package ru.panina.springproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.panina.springproject.dao.BookDAO;
 
 @Controller
+@RequestMapping("/books")
 public class BooksController {
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
 }

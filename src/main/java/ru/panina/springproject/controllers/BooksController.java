cdd824e488f93cfa25fa810d10a6f7ad56d18dad
katerina <katerina_panina1@mail.ru>
2024-09-02 21:03:58 +0300

    add methods for create page and update page

--- src/main/java/ru/panina/springproject/controllers/BooksController.java
+++ src/main/java/ru/panina/springproject/controllers/BooksController.java
@@ -1,24 +1,55 @@
 package ru.panina.springproject.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import ru.panina.springproject.dao.BookDAO;
+import ru.panina.springproject.dao.PersonDAO;
+import ru.panina.springproject.models.Book;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
     private final BookDAO bookDAO;
+    private final PersonDAO personDAO;
 
     @Autowired
-    public BooksController(BookDAO bookDAO) {
+    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
     }
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "books/index";
+    }
+    @GetMapping("/new")
+    public String newBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "books/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute @Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "books/new";
+        }
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model){
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute @Valid Book book, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "books/edit";
+        }
+        bookDAO.update(id, book);
+        return "redirect:/books";
     }
 }

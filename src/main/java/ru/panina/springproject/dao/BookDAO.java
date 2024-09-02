e2dcf71826968c1516c0133558d3391cdf3fb3ac
katerina <katerina_panina1@mail.ru>
2024-09-02 13:58:31 +0300

    create dao and add query to select all books from database

--- src/main/java/ru/panina/springproject/dao/BookDAO.java
+++ src/main/java/ru/panina/springproject/dao/BookDAO.java
@@ -1,5 +1,24 @@
 package ru.panina.springproject.dao;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.panina.springproject.models.Book;
+
+import java.util.List;
+
+@Component
 public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index(){
+        return jdbcTemplate.query("select * from book", new BeanPropertyRowMapper<Book>(Book.class));
+    }
 }

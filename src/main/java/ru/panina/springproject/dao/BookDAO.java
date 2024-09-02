6f4c87a6c2695df5449a358ac1e559c0f871fe73
katerina <katerina_panina1@mail.ru>
2024-09-02 21:02:45 +0300

    add save, show and update methods for database

--- src/main/java/ru/panina/springproject/dao/BookDAO.java
+++ src/main/java/ru/panina/springproject/dao/BookDAO.java
@@ -19,6 +19,18 @@ public class BookDAO {
     }
 
     public List<Book> index(){
-        return jdbcTemplate.query("select * from book", new BeanPropertyRowMapper<Book>(Book.class));
+        return jdbcTemplate.query("select * from book", new BookMapper());
+    }
+    public void save(Book book){
+        jdbcTemplate.update("insert into book(title, author, year) values(?, ?, ?)",
+                book.getTitle(), book.getAuthor(), book.getYear());
+    }
+    public Book show(int id){
+        return jdbcTemplate.query("select * from book where id = ?", new Object[]{id},
+                        new BookMapper()).stream().findAny().orElse(null);
+    }
+    public void update(int id, Book book){
+        jdbcTemplate.update("update book set title = ?, author = ?, year = ? where id = ?",
+                book.getTitle(), book.getAuthor(), book.getYear(), id);
     }
 }
